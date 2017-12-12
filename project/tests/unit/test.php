153d77ae2d7d4ddccd732815b9ca8c34cfeca347
minlu1021 <mlu821@bu.edu>
2017-12-12 17:43:33 -0500

    unitTest

---
+++ project/tests/unit/test.php
@@ -0,0 +1,132 @@
+<?php
+
+// Author: Lu Min
+// Unit test 
+
+class test extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+		
+    }
+	
+	function testSavingUser()
+	{
+		
+	}
+	
+	function testUserNameCanBeChanged()
+	{
+		
+	}
+	
+	function testUserNameCanBeChanged()
+	{
+		
+	}
+	
+	function testAddUserStory()
+	{
+
+	}
+	
+	function testDeleteUserStory()
+	{
+		
+	}
+	
+	function testAdvancedAddStory()
+	{
+		
+	}
+	
+	function testSortByHand()
+	{
+		
+	}
+	
+	function testSortByDate()
+	{
+		
+	}
+	
+	function testSortByModified()
+	{
+		
+	}
+	
+	function testSortByPoint()
+	{
+		
+	}
+	
+	function testSearch()
+	{
+		
+	}
+	
+	function testAddDescription()
+	{
+		
+	}
+	
+	function testAddSprint()
+	{
+		
+	}
+	
+	function testChangeSprint()
+	{
+		
+	}
+	
+	function testShowDescription()
+	{
+		
+	}
+	
+	function testHideDescription()
+	{
+		
+	}
+	
+	function testShowTag()
+	{
+		
+	}
+	
+	function testDeleteTag()
+	{
+		
+	}
+	
+	function testClear()
+	{
+		
+	}
+	
+	function testShowCompletedStory()
+	{
+		
+	}
+	
+	function testLogout()
+	{
+		
+	}
+	
+}
